8ab049c00ce4e6b7da7f1037f24006ca7236884e
Maryna Delidova <marynadelidova@marynas-macbook-pro.local>
2018-04-11 21:17:48 +0300

    Initial Commit

---
+++ DeligatesTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeligatesTest
+//
+//  Created by Maryna Delidova on 4/11/18.
+//  Copyright © 2018 Maryna Delidova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeligatesTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeligatesTest
+//
+//  Created by Maryna Delidova on 4/11/18.
+//  Copyright © 2018 Maryna Delidova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
